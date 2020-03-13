16031e482b01bf3c5a3d570f54f26d3beea76917
roshan1718 <shinderoshan.2902@gmail.com>
2020-03-13 14:44:15 +0530

    ADDED code using case statement

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -x
-
 isPartTime=1
 isFullTime=2
 empRatePerHr=20
-number1=$(( RANDOM % 3 ))
+number1=$(( RANDOM%3 ))
 
-if [ $isFullTime -eq $number1	]
-then
-	empHrs=8
-elif [ $isPartTime -eq $number1 ]
-then
-	empHrs=8
-else
-	empHrs=0
-fi
-salary=$(( $empHrs * $empRatePerHr ))
+case $number1 in
+		$isFullTime)
+			empHrs=8 ;;
+
+		$isPartTime)
+			empHrs=4 ;;
+		
+		*)
+			empHrs=0 ;;
+esac
+
+salary=$(($empHrs * $empRatePerHr))
+
